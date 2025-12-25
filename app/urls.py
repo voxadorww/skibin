c47f8e3373cff468f4fcec166cfca73f4e1e30a2
voxadorww <voxadorba@gmail.com>
2025-12-25 22:32:26 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("new", views.create_paste, name="create_paste"),
+    path("contact", views.contact, name="contact"),
+    path("terms", views.terms, name="terms"),
     path('p/<str:paste_id>/', views.view_paste, name='view_paste'),
     path('p/<str:paste_id>/raw/', views.raw_paste, name='raw_paste'),
     path('p/<str:paste_id>/clone/', views.clone_paste, name='clone_paste'),
