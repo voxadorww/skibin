98d95c541f851e03fd3ff5bdee81244e936d7906
voxadorww <voxadorba@gmail.com>
2025-12-26 16:01:47 +0100

    Update settings.py

--- skibin/settings.py
+++ skibin/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'skibin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'HOST': os.environ.get('PGHOST'),
+        'PORT': os.environ.get('PGPORT'),
     }
 }
 
