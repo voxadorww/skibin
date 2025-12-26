34491fbf7f4d4a56968e275a18410551e6e0426b
voxadorww <voxadorba@gmail.com>
2025-12-26 20:23:25 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('ads.txt', views.ads_txt, name='ads_txt'),
     path("advtest", views.advtest, name="advtest"),
     path("contact", views.contact, name="contact"),
+    path("pricing, views.pricing, name="pricing"),
     path("terms", views.terms, name="terms"),
     path('p/<str:paste_id>/', views.view_paste, name='view_paste'),
     path('p/<str:paste_id>/raw/', views.raw_paste, name='raw_paste'),
